cbc89ee61e4aae805448816846c8206beabf1237
Steven Bengtson <steven.bengtson@me.com>
2018-06-24 15:17:40 -0700

    Clean up app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CalculatorComponent } from './calculator/calculator.component';
+
+const routes: Routes = [
+  { path: '', component: CalculatorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
